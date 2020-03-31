038031c37c7d2b4e9585f78575c0d3d264043c04
Raxa Hansana <raxah@Raxas-MacBook-Pro.local>
2020-03-31 14:48:18 -0700

    finished step 8

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class About extends Component {
   render() {
     return (
       <div>
         <div className='subnav'>
+          <Link path='/about' className='subnav_links'>
+            <h3>About</h3>
+          </Link>
+          <Link path='/about/history' className='subnav_links'>
+            <h3>History</h3>
+          </Link>
+          <Link path='about/contact' className='subnav_links'>
+            <h3>Contact</h3>
+          </Link>
         </div>
-        <div className='box'>
-        </div>
+        <div className='box'></div>
       </div>
-    )
+    );
   }
 }
